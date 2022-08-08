1f5ce9f85cc5ccaafab00e953db69e87f2619b77
Beryl B <beriebaldwin@gmail.com>
2022-08-07 20:56:35 -0400

    add about page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('poptarts/', views.poptarts_index, name='poptarts_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.http import HttpResponse
+
+
+class Poptart:
+  def __init__(self, flavor, description):
+    self.flavor = flavor
+    self.description = description
+
+poptarts = [
+  Poptart('Strawberry', "A true classic. Kind of boring but also she's always there for you"),
+  Poptart('Brown Sugar Cinnamon', "Everyone's favorite and she knows it."),
+  Poptart('Wild Berry', "Strawberry's cooky cousin."),
+  Poptart('Hot Fudge Sundae', "A truly decadent choice for breakfast. Kind of a red flag honestly.")
+]
+
+def home(request):
+  return HttpResponse('<h1>Hi</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def poptarts_index(request):
+  return render(request, 'poptarts/index.html', { 'poptarts': poptarts })
