bec063329159053bb1c8eae455704d3bdcb82355
Beryl B <beriebaldwin@gmail.com>
2022-08-07 22:49:14 -0400

    add emotions

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Poptart
+from .models import Poptart, Emotion
 
 
 admin.site.register(Poptart)
+admin.site.register(Emotion)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+FEELINGS = (
+  ('R', 'Regret'),
+  ('A', 'Ambivalence'),
+  ('S', 'Satisfied')
+)
+
 class Poptart(models.Model):
   flavor = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
@@ -10,4 +16,22 @@ class Poptart(models.Model):
 
   def get_absolute_url(self):
       return reverse("poptarts_detail", kwargs={"poptart_id": self.id})
+
+
+
+class Emotion(models.Model):
+  date = models.DateField('Date eaten')
+  feeling = models.CharField(
+    max_length=1,
+    choices=FEELINGS,
+    default=FEELINGS[0][0]
+    )
+
+  poptart = models.ForeignKey(Poptart, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_feeling_display()} on {self.date}"
+
+
+
 
